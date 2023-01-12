76de6cb9f9b236b32aadfca9f66a688911dda4b2
AA5756 <105878893+AA5756@users.noreply.github.com>
2023-01-12 11:24:06 -0800

    Add files via upload

--- export_final_qual.sh
+++ export_final_qual.sh
@@ -17,9 +17,21 @@ echo "------------------copy from local to remote----------------"
 sshpass -p "$password" ssh $serial mkdir -p /home/fetch/$number
 
 #-----------------copy from local home directory to remote home directory----------------
+sshpass -p "$password" scp -o StrictHostKeyChecking=no /home/fetch/$number/calibrated_urdf.urdf $serial:/home/fetch
+
 sshpass -p "$password" scp -o StrictHostKeyChecking=no /home/fetch/$number/camera_extrinsic_calibration_date $serial:/home/fetch
+
+sshpass -p "$password" scp -o StrictHostKeyChecking=no /home/fetch/$number/robot.launch $serial:/home/fetch
+
+sshpass -p "$password" scp -o StrictHostKeyChecking=no /home/fetch/$number/noetic_robot.launch $serial:/home/fetch
 
 #-----------------copy from remote home directory to remote fetchcore directory----------------
 sshpass -p "$password" ssh -t -o StrictHostKeyChecking=no fetch@freight100-1904 "echo $password | sudo -S mv /home/fetch/camera_extrinsic_calibration_date /etc/fetchcore"
+
+sshpass -p "$password" ssh -t -o StrictHostKeyChecking=no fetch@freight100-1904 "echo $password | sudo -S mv /home/fetch/calibrated_urdf.urdf /etc/fetchcore"
+
+sshpass -p "$password" ssh -t -o StrictHostKeyChecking=no fetch@freight100-1904 "echo $password | sudo -S mv /home/fetch/robot.launch /etc/ros/indigo"
+
+sshpass -p "$password" ssh -t -o StrictHostKeyChecking=no fetch@freight100-1904 "echo $password | sudo -S mv /home/fetch/noetic_robot.launch /etc/ros/noetic/robot.launch"
 
 echo "Copying Files are Completed"
