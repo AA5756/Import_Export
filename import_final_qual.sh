3391207ab45e770467cdc24038257cf57617cde5
AA5756 <105878893+AA5756@users.noreply.github.com>
2023-01-12 11:26:19 -0800

    Update import_final_qual.sh

--- import_final_qual.sh
+++ import_final_qual.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-# A Simple Bash Script
+# Base Script to import calibration files for final qual from appropriate robot locations to local desktop
+# This Script only being used when the robot is final qualed and want to qualed again
+# this Script should perform before Reimage the Robot
+
+# Created by Abhi Adhuria
+
 read -p "Enter Robot Serial Number: " serial
 read -p "Enter Four Digit Robot Number: " number
 read -p "Enter Lastpass Password: " password
+
 ## Follownig Code create directory in local machine by Robot Serial Number date
 mkdir -p /home/fetch/$number
+
 #Following Code is to get Copy Extrinsic and Gyro Calibration file from Robot to local mahcine 
 echo $serial
 sshpass -p "$password" scp -o StrictHostKeyChecking=no $serial:/etc/fetchcore/calibrated_urdf.urdf /home/fetch/$number/
