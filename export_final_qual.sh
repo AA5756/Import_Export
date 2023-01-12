f1c683f0ccf1f3615016a08d8d21161ea5621ada
AA5756 <105878893+AA5756@users.noreply.github.com>
2023-01-12 11:01:07 -0800

    Add files via upload

--- export_final_qual.sh
+++ export_final_qual.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-# A Simple Bash Script
+# Base Script to export calibration files for final qual to appropriate robot locations
+# This Script only being used when the robot is final qualed and want to qualed again
+# this Script should perform after Reimage the Robot
+
+# Created by Abhi Adhuria
+
 read -p "Enter Robot Serial Number: " serial
 read -p "Enter Four Digit Robot Number: " number
 read -p "Enter Lastpass Password: " password
@@ -12,9 +17,9 @@ echo "------------------copy from local to remote----------------"
 sshpass -p "$password" ssh $serial mkdir -p /home/fetch/$number
 
 #-----------------copy from local home directory to remote home directory----------------
-sshpass -p "$password" scp -o StrictHostKeyChecking=no /home/fetch/$number/camera_extrinsic_calibration_date $serial:/home/fetch/$number
+sshpass -p "$password" scp -o StrictHostKeyChecking=no /home/fetch/$number/camera_extrinsic_calibration_date $serial:/home/fetch
 
 #-----------------copy from remote home directory to remote fetchcore directory----------------
-ssh -t fetch@freight100-1904 "sudo mv /home/fetch/camera_extrinsic_calibration_date /etc/fetchcore"
+sshpass -p "$password" ssh -t -o StrictHostKeyChecking=no fetch@freight100-1904 "echo $password | sudo -S mv /home/fetch/camera_extrinsic_calibration_date /etc/fetchcore"
 
 echo "Copying Files are Completed"
